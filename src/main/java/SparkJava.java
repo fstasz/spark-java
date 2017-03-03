e78b41e1221667dd8c0b7443e11ba6229561c99e
fstasz <fs4364@gmail.com>
2017-03-03 15:02:10 -0600

    Add search string constant.

--- src/main/java/SparkJava.java
+++ src/main/java/SparkJava.java
@@ -24,6 +24,7 @@ import org.apache.spark.SparkConf;
 public class SparkJava {
 
     private static final String APP_NAME = "SparkJava";
+    private static final String SEARCH_STRING = "spark";
     private static final String SPARK_MASTER = "local[*]";
     private static final String WORDS_FILE = "/usr/share/dict/words";
 
@@ -35,8 +36,8 @@ public class SparkJava {
 
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        // Define RDD from os x words file and return words containing SearchString
-        JavaRDD<String> wordList = sc.textFile(WORDS_FILE).filter(s -> s.contains("spark"));
+        // Define RDD from OS X words file and return words containing SearchString
+        JavaRDD<String> wordList = sc.textFile(WORDS_FILE).filter(s -> s.contains(SEARCH_STRING));
 
         sc.stop();
     }
